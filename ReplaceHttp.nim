688834290d5d7f66568cf4993fa5e828d2ac6b6c
notoiro297 <notoiro297@gmail.com>
2024-01-06 00:05:11 +0900

    add dict duplication check and sort

--- ReplaceHttp.nim
+++ ReplaceHttp.nim
@@ -6,6 +6,11 @@ import std/re
 import unicode
 import std/times
 import prologue
+import std/os
+import std/dirs
+import std/paths
+import std/files
+import algorithm
 
 type DictData* = ref object
   from_re*: re.Regex
@@ -13,25 +18,49 @@ type DictData* = ref object
 
 var dict: seq[DictData]
 
-proc load_dict*(path = "./dict.json"): seq[DictData] =
+proc load_dict_dir*(dict_path = "./dicts/"): seq[DictData] =
+  var path = paths.Path(dict_path)
+
+  if(not dirExists(path)):
+    echo "Dictionary file does not exist!"
+    quit(1)
+
+  var tmp_dict_arr: seq[seq[string]]
+
+  for file in walkDirRec(path):
+    if(not fileExists(file)): continue
+
+    try:
+      var json_file = parseFile(string(file))
+      var dict = json_file["dict"].getElems
+
+      for dic in dict:
+        var s = dic[0].getStr
+        if(not tmp_dict_arr.any((d) => d[0] == s)):
+          tmp_dict_arr.add(@[dic[0].getStr, dic[1].getStr])
+
+      echo "loaded dict file: " & string(file)
+    except:
+      echo "load err file: " & string(file)
+      echo repr(getCurrentException())
+      quit(1)
+
+  tmp_dict_arr.sort((a, b) => cmp(a[0].toRunes.len, b[0].toRunes.len))
+  tmp_dict_arr.reverse()
+
+  var result_arr: seq[DictData]
+
   try:
-    var json_file = parseFile(path)
+    for dic in tmp_dict_arr:
+      var from_re = re(escapeRe(dic[0]), flags = {reIgnoreCase, reStudy})
 
-    var dict = json_file["dict"].getElems
-    var dict_arr: seq[DictData];
-
-    for dic in dict:
-      var from_re = re(escapeRe(dic[0].getStr), flags = {reIgnoreCase, reStudy})
-      var to = dic[1].getStr
-
-      dict_arr.add(DictData(from_re: from_re, to: to))
-
-    result = dict_arr
-
-  except IOError, JsonParsingError:
-    quit(1)
+      result_arr.add(DictData(from_re: from_re, to: dic[1]))
   except:
+    echo "regex parse error"
+    echo repr(getCurrentException())
     quit(1)
+
+  result = result_arr
 
 proc engtokana(text: string): string {.gcsafe.} =
   var tmp_text = text
@@ -54,7 +83,7 @@ proc onRequest*(ctx: Context): Future[void] {.async, gcsafe.} =
 
 proc main() {.async.} =
   echo "start"
-  dict = load_dict()
+  dict = load_dict_dir()
   echo "dict loaded"
 
   var settings = newSettings(port = Port(2972))
